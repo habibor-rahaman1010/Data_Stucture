c9e3d0cb020e1db76d89940b415813cb77e1145b
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-24 20:34:12 +0600

    hello programmer

--- Sort_Algorithm/program-1.cpp
+++ Sort_Algorithm/program-1.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 vector<int> Selection_Sort(vector<int>ara){
     int min_index;
+    int temp;
+    // [12, 34, 13, 3, 45, 23, 12, 22, 47, 18]
     for(int i = 0; i < ara.size() - 1; i++){
         for(int j = i + 1; j < ara.size(); j++){
             min_index = i;
@@ -13,19 +15,23 @@ vector<int> Selection_Sort(vector<int>ara){
                 min_index = j;
             }
             if(min_index != i){
-                swap(ara[min_index], ara[i]);
+                temp = ara[min_index];
+                ara[min_index] = ara[i];
+                ara[i] = temp;
             }
         }
     }
+    return ara;
 }
 
 int main() {
     vector<int>ara = {12, 34, 13, 3, 45, 23, 12, 22, 47, 18};
     vector<int> vect =  Selection_Sort(ara);
 
-    for(int i = 0; i < ara.size(); i++){
-        cout<<ara[i] <<" ";
+    for(int i = 0; i < vect.size(); i++){
+        cout<<vect[i] <<" ";
     }
+    cout<<"\n";
 
 return 0;
 }
