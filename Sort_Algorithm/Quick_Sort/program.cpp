2daf7a1590a075012ecc305efb2cbeaccf14ff9a
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-25 19:42:42 +0600

    ok man

---
+++ Sort_Algorithm/Quick_Sort/program.cpp
@@ -0,0 +1,11 @@
+//quick sort implement in c++ program
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int>ara = {{11, 9, 3, 15, 14, 18, 12, 22, 34, 56, 21}};
+
+return 0;
+}
