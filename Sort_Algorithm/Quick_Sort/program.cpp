732cb9d82f8c8cdedd5a400a98796c6d45bed89e
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-25 19:50:10 +0600

    recape data structure in c++ program

--- Sort_Algorithm/Quick_Sort/program.cpp
+++ Sort_Algorithm/Quick_Sort/program.cpp
@@ -4,8 +4,32 @@
 #include <vector>
 using namespace std;
 
+vector<int> Quic_Sort(vector<int>ara) {
+    if(ara.size() <= 1){
+        return ara;
+    }
+    int pivot = ara.size() / 2;
+
+    vector<int>left, right;
+    for(int i = 0; i < ara.size(); i++){
+        if(i == pivot){
+            continue;
+        }
+        if(ara[i] <= pivot){
+            left.push_back(ara[i]);
+        }
+        if(ara[i] > pivot){
+            right.push_back(ara[i]);
+        }
+    }
+    vector<int>sorted_left = Quic_Sort(left);
+    vector<int>sorted_right = Quic_Sort(right);
+
+}
+
 int main() {
-    vector<int>ara = {{11, 9, 3, 15, 14, 18, 12, 22, 34, 56, 21}};
+    vector<int>ara = {11, 9, 3, 15, 14, 18, 12, 22, 34, 56, 21};
+    vector<int>vect = Quic_Sort(ara);
 
 return 0;
 }
