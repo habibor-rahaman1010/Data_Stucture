854f2b38d893aca08b909e6cac2089a9e108e227
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-25 11:46:30 +0600

    ok

---
+++ Sort_Algorithm/Bubble_Sort/program-3.cpp
@@ -0,0 +1,36 @@
+//bubble sort in c++ program...
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> Bubble_Sort(vector<int>ara){
+    for(int i = 0; i < ara.size(); i++){
+        int last = (ara.size() - 1) - i;
+        bool sorted = true;
+        for(int j = 0; j <= last - 1; j++){
+            if(ara[j] > ara[last]){
+                int temp = ara[j];
+                ara[j] = ara[last];
+                ara[last] = temp;
+                sorted = false;
+            }
+        }
+        if(sorted){
+            break;
+        }
+    }
+    return ara;
+}
+
+int main() {
+    vector<int>ara = {12, 23, 6, 8, 11, 29, 50, 33, 3, 1};
+    vector<int>vect = Bubble_Sort(ara);
+
+    for(int i = 0; i < vect.size(); i++){
+        cout<<vect[i] <<" ";
+    }
+    cout<<"\n";
+
+return 0;
+}
