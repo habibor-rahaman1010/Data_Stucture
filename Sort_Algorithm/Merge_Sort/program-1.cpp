2f0c1881603970297d191a55354dce1826bfda6a
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-25 18:49:26 +0600

    ok man

--- Sort_Algorithm/Merge_Sort/program-1.cpp
+++ Sort_Algorithm/Merge_Sort/program-1.cpp
@@ -28,13 +28,34 @@ vector<int>Merge_Sort(vector<int>ara){
     int index2 = 0;
     
     for(int i = 0; i < ara.size(); i++){
-        
+        if(index1 == left_sorted.size()){
+            sorted_array.push_back(right_sorted[index2]);
+            index2++;
+        }
+        else if(index2 == right_sorted.size()){
+            sorted_array.push_back(left_sorted[index1]);
+            index1++;
+        }
+        else if(left_sorted[index1] < right_sorted[index2]){
+            sorted_array.push_back(left_sorted[index1]);
+            index1++;
+        }
+        else{
+            sorted_array.push_back(right_sorted[index2]);
+            index2++;
+        }
     }
+    return sorted_array;
 }
 
 int main() {
     vector<int>ara = {11, 9, 3, 15, 14, 18, 12, 22, 34, 56, 21};
     vector<int>vect = Merge_Sort(ara);
+
+    for(int i = 0; i < vect.size(); i++){
+        cout<<vect[i] <<" ";
+    }
+    cout<<"\n";
 
 return 0;
 }
