502c2e97eb5b22bedaa558232a614e0f372028df
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-26 18:55:52 +0600

    ok

---
+++ LInked_List/program-1.cpp
@@ -0,0 +1,24 @@
+//Linked list implement in c++ program...
+
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *next;
+};
+
+class LinkedList {
+    public:
+        Node * head;
+        LinkedList(){
+            head = NULL;
+        }
+};
+
+int main() {
+
+
+return 0;
+}
